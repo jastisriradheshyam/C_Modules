843407ed75e63bf9b667b1edfefaf85a123a048b
Jasti Sri Radhe Shyam <jastisriradheshyam@users.noreply.github.com>
2017-11-11 21:12:01 +0530

    Update duplicate_remover.h
    added function for float and double type.

--- duplicate_remover/duplicate_remover.h
+++ duplicate_remover/duplicate_remover.h
@@ -11,6 +11,8 @@ int compare (const void* p1, const void* p2)
 int *dup_rem(int*,int*);
 int *dup_rem_sort(int*,int*);
 char *char_dup_rem(char*,int*);
+float *float_dup_rem(float*,int*);
+double *double_dup_rem(double*,int*);
 int *dup_rem(int *arr,int *no)
 {
     int i,j,k=0;
@@ -90,6 +92,78 @@ char *char_dup_rem(char *arr,int *no)
     int n = *no;
     short *sup = calloc(n,sizeof(short));
     char *anarr = calloc(n,sizeof(char));
+    for(i=0;i<n;i++)
+    {
+     if(sup[i]!=1)
+     {
+      for(j=i+1;j<n;j++)
+      {
+       if(arr[i]==arr[j])
+       {
+        sup[i]=1;
+        sup[j]=1;
+       }
+      }
+     }
+     else{continue;}
+    }
+    for(i=0;i<n;i++)
+    {
+     if(sup[i]==0)
+     {
+      anarr[k]=arr[i];
+      k++;
+     }
+    }
+
+    free(sup);
+    *no = k;
+    free(arr);
+    return anarr;
+}
+
+float *float_dup_rem(float *arr,int *no)
+{
+    int i,j,k=0;
+    int n = *no;
+    short *sup = calloc(n,sizeof(short));
+    float *anarr = calloc(n,sizeof(float));
+    for(i=0;i<n;i++)
+    {
+     if(sup[i]!=1)
+     {
+      for(j=i+1;j<n;j++)
+      {
+       if(arr[i]==arr[j])
+       {
+        sup[i]=1;
+        sup[j]=1;
+       }
+      }
+     }
+     else{continue;}
+    }
+    for(i=0;i<n;i++)
+    {
+     if(sup[i]==0)
+     {
+      anarr[k]=arr[i];
+      k++;
+     }
+    }
+
+    free(sup);
+    *no = k;
+    free(arr);
+    return anarr;
+}
+
+double *double_dup_rem(double *arr,int *no)
+{
+    int i,j,k=0;
+    int n = *no;
+    short *sup = calloc(n,sizeof(short));
+    double *anarr = calloc(n,sizeof(double));
     for(i=0;i<n;i++)
     {
      if(sup[i]!=1)
